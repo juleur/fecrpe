6e4a2e79df84cad486117a8b00aedfa68bb6934d
Lesieur Julien <lesieur.julien@tuta.io>
2019-07-06 10:14:41 +0200

    feat(auth-login) login form added

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent, SignupComponent
+  ],
   imports: [
-    CommonModule,
-    AuthRoutingModule
+    CommonModule, AuthRoutingModule, FormsModule, ReactiveFormsModule,
+  ],
+  exports: [
+    FormsModule, ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
